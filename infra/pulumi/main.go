d5e90c3f65c59480b672bb2c4ac976144197802e
carlosEA28 <carloseaduarte28@gmail.com>
2026-03-22 10:21:06 -0300

    fix: add RabbitMQ environment variables and update Dockerfiles for services

--- infra/pulumi/main.go
+++ infra/pulumi/main.go
@@ -98,7 +98,6 @@ func main() {
 		dbEndpoints := make(map[string]pulumi.StringOutput)
 
 		for service, dbName := range dbServices {
-
 			sg, err := ec2.NewSecurityGroup(ctx, fmt.Sprintf("rds-sg-%s", service), &ec2.SecurityGroupArgs{
 				Ingress: ec2.SecurityGroupIngressArray{
 					&ec2.SecurityGroupIngressArgs{
@@ -139,21 +138,87 @@ func main() {
 			return err
 		}
 
+		// RabbitMQ
+		rabbitmqUser := os.Getenv("RABBITMQ_USER")
+		rabbitmqPass := os.Getenv("RABBITMQ_PASS")
+
+		_, err = ecsx.NewFargateService(ctx, "ecs-service-rabbitmq", &ecsx.FargateServiceArgs{
+			Cluster:        cluster.Arn,
+			DesiredCount:   pulumi.Int(1),
+			AssignPublicIp: pulumi.Bool(true),
+			TaskDefinitionArgs: &ecsx.FargateServiceTaskDefinitionArgs{
+				Containers: map[string]ecsx.TaskDefinitionContainerDefinitionArgs{
+					"rabbitmq": {
+						Image:  pulumi.String("rabbitmq:3.13-management-alpine"),
+						Cpu:    pulumi.Int(256),
+						Memory: pulumi.Int(512),
+						Environment: ecsx.TaskDefinitionKeyValuePairArray{
+							ecsx.TaskDefinitionKeyValuePairArgs{
+								Name:  pulumi.String("RABBITMQ_DEFAULT_USER"),
+								Value: pulumi.String(rabbitmqUser),
+							},
+							ecsx.TaskDefinitionKeyValuePairArgs{
+								Name:  pulumi.String("RABBITMQ_DEFAULT_PASS"),
+								Value: pulumi.String(rabbitmqPass),
+							},
+						},
+						PortMappings: ecsx.TaskDefinitionPortMappingArray{
+							&ecsx.TaskDefinitionPortMappingArgs{ContainerPort: pulumi.Int(5672)},
+							&ecsx.TaskDefinitionPortMappingArgs{ContainerPort: pulumi.Int(15672)},
+						},
+					},
+				},
+			},
+		}, nil)
+		if err != nil {
+			return err
+		}
+
+		// Serviços
 		for service, imageRef := range imageRefs {
 			containerName := service
 
 			var env ecsx.TaskDefinitionKeyValuePairArray
 			if dbEndpoint, ok := dbEndpoints[service]; ok {
-				dbUrl := pulumi.Sprintf("postgresql://postgres:%s@%s/%s",
-					os.Getenv("DB_PASSWORD"),
-					dbEndpoint,
-					dbServices[service],
-				)
-				env = ecsx.TaskDefinitionKeyValuePairArray{
-					ecsx.TaskDefinitionKeyValuePairArgs{
-						Name:  pulumi.String("DATABASE_URL"),
-						Value: dbUrl,
-					},
+				if service == "payment-service" {
+					env = ecsx.TaskDefinitionKeyValuePairArray{
+						ecsx.TaskDefinitionKeyValuePairArgs{
+							Name:  pulumi.String("DB_HOST"),
+							Value: dbEndpoint,
+						},
+						ecsx.TaskDefinitionKeyValuePairArgs{
+							Name:  pulumi.String("DB_PORT"),
+							Value: pulumi.String("5432"),
+						},
+						ecsx.TaskDefinitionKeyValuePairArgs{
+							Name:  pulumi.String("DB_USER"),
+							Value: pulumi.String("postgres"),
+						},
+						ecsx.TaskDefinitionKeyValuePairArgs{
+							Name:  pulumi.String("DB_PASSWORD"),
+							Value: pulumi.String(os.Getenv("DB_PASSWORD")),
+						},
+						ecsx.TaskDefinitionKeyValuePairArgs{
+							Name:  pulumi.String("DB_NAME"),
+							Value: pulumi.String(dbServices[service]),
+						},
+						ecsx.TaskDefinitionKeyValuePairArgs{
+							Name:  pulumi.String("DB_SSL_MODE"),
+							Value: pulumi.String("require"),
+						},
+					}
+				} else {
+					dbUrl := pulumi.Sprintf("postgresql://postgres:%s@%s/%s",
+						os.Getenv("DB_PASSWORD"),
+						dbEndpoint,
+						dbServices[service],
+					)
+					env = ecsx.TaskDefinitionKeyValuePairArray{
+						ecsx.TaskDefinitionKeyValuePairArgs{
+							Name:  pulumi.String("DATABASE_URL"),
+							Value: dbUrl,
+						},
+					}
 				}
 			}
 
@@ -176,6 +241,7 @@ func main() {
 				return err
 			}
 		}
+
 		return nil
 	})
 }
